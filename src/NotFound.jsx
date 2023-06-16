2762f4c4e6c86cc92fc4cee5c13fde5dc709ab5f
Ayush Katoch <katochayushhp@gmail.com>
2023-06-16 19:24:41 +0530

    Create NotFound.jsx

---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = ({selectedTeam, teamMemberCount}) => {
+    return (
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+            <div className="col-8">
+            <h1 class="text-danger">404 - Page Not found</h1>
+        </div>
+        </div>
+        </header>
+    )
+}
+export default NotFound;
