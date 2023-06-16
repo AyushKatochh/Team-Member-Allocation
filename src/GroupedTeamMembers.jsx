25a426304168d17ee0cc50969c08becf9f988bbc
AyushKatochh <katochayushhp@gmail.com>
2023-06-16 18:00:16 +0530

    first commit

---
+++ src/GroupedTeamMembers.jsx
@@ -0,0 +1,13 @@
+const GroupedTeamMembers = ({selectedTeam, teamMemberCount}) => {
+    return (
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+            <div className="col-8">
+            <h1>Grouped Team Members</h1>
+            <h3>{selectedTeam} has {teamMemberCount} Members</h3>
+        </div>
+        </div>
+        </header>
+    )
+}
+export default GroupedTeamMembers;
